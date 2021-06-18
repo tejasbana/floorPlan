e72c1a7f2d2002cc14d8407a8c3c146d2abefbb0
tejasbana <tejasbana@gmail.com>
2021-06-18 16:45:28 +0530

    deploy

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ import PIL
 from PIL import Image
 # from Model import UNet 
 import pickle 
-from flask import Flask, flash, render_template, request
+from flask import Flask, flash, render_template, request, url_for
 # please note the import from `flask_uploads` - not `flask_reuploaded`!!
 # this is done on purpose to stay compatible with `Flask-Uploads`
 from flask_uploads import IMAGES, UploadSet, configure_uploads
+from flask_cors import CORS
 # # Make url public for colab
 # from flask_ngrok import run_with_ngrok
+from werkzeug.utils import secure_filename
 import gdown
 
 if not os.path.isfile("./model_pickle.sav"):
@@ -28,14 +30,23 @@ if not os.path.isfile("./model_pickle.sav"):
     output = 'model_pickle.sav'
     gdown.download(url, output, quiet=False) 
 
-
 app = Flask(__name__, static_folder='generated')
 photos = UploadSet("photos", IMAGES)
 app.config["UPLOADED_PHOTOS_DEST"] = "images"
 app.config["SECRET_KEY"] = os.urandom(24)
 configure_uploads(app, photos)
 
+
+UPLOAD_FOLDER = './images'
+ALLOWED_EXTENSIONS = {'png'}
+
+# app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 test_dir ="./"
+
+CORS(app)
 
 # Start ngrok when the app is running
 # run_with_ngrok(app)
@@ -43,54 +54,76 @@ test_dir ="./"
 def denorm(img_tensor):
     return img_tensor*0.5 + 0.5
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route("/", methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST' and 'photo' in request.files:
-        os.system('rm -rf images')
-        os.system('rm -rf generated')
-        os.system('mkdir images')
-        os.system('mkdir generated')
+    if request.method == 'POST' :
+        print("this is request.files:")
+        print(request.files)
+        if 'photo' in request.files:
 
-        photos.save(request.files['photo'])
-        flash("Photo saved successfully.", "p")
-        # img = cv2.imread('images/'+str(request.files['photo'].filename))
-        # img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            print("request has photo!!")
+            os.system('rm -rf images')
+            os.system('rm -rf generated')
+            os.system('mkdir images')
+            os.system('mkdir generated')
 
-        image = Image.open('images/'+str(request.files['photo'].filename))
-        image = image.convert("RGB")
-        transform=Compose([ 
-                            tt.Resize((256,256),interpolation=Image.ANTIALIAS),
-                            tt.CenterCrop(256),
-                            tt.ToTensor(),
-                            tt.Normalize(mean=(0.5,), std=(0.5,))])
+            photo = request.files['photo']
+            filename = secure_filename(photo.filename)
+            photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #photos.save(request.files['photo'])
+            
+            flash("Photo saved successfully.", "p")
+            # img = cv2.imread('images/'+str(request.files['photo'].filename))
+            # img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+            image = Image.open('./images/'+filename)
+
+            image = image.convert("RGB")
+            transform=Compose([ 
+                                tt.Resize((256,256),interpolation=Image.ANTIALIAS),
+                                tt.CenterCrop(256),
+                                tt.ToTensor(),
+                                tt.Normalize(mean=(0.5,), std=(0.5,))])
 
 
-        image = transform(image)
-        image = torch.unsqueeze(image, 0)
+            image = transform(image)
+            image = torch.unsqueeze(image, 0)
 
-        # Load pretrained model
-        # model = UNet(True)
-        # model.load_state_dict(torch.load("./generator.pth",map_location=torch.device('cpu')))
-        # Rather use pickel model
-        filename = 'model_pickle.sav'
-        model = pickle.load(open(filename, 'rb'))
+            # Load pretrained model
+            # model = UNet(True)
+            # model.load_state_dict(torch.load("./generator.pth",map_location=torch.device('cpu')))
+            # Rather use pickel model
+            filename = 'model_pickle.sav'
+            model = pickle.load(open(filename, 'rb'))
+            prediction = model(image).detach()
+            prediction = denorm(prediction.squeeze(0))
+            fname = "/test-images.png"
+            save_image(prediction, test_dir + fname)
+            prediction = prediction.permute(1,2,0).numpy()
+            # plt.imshow(image)
+            
+            gen_path_to_save = "generated/"+str(request.files['photo'].filename)
+            orig_path_to_save = "generated/orig"+str(request.files['photo'].filename)
+            plt.imsave(gen_path_to_save, prediction)
+            plt.imsave(orig_path_to_save, denorm(image.squeeze(0)).permute(1,2,0).numpy())
+            flash("Processed Successfully", "p")
+            path_to_save = [orig_path_to_save, gen_path_to_save]
 
-        prediction = model(image).detach()
-        prediction = denorm(prediction.squeeze(0))
-        fname = "/test-images.png"
-        save_image(prediction, test_dir + fname)
-        prediction = prediction.permute(1,2,0).numpy()
-        # plt.imshow(image)
-        
-        gen_path_to_save = "generated/"+str(request.files['photo'].filename)
-        orig_path_to_save = "generated/orig"+str(request.files['photo'].filename)
-        plt.imsave(gen_path_to_save, prediction)
-        plt.imsave(orig_path_to_save, denorm(image.squeeze(0)).permute(1,2,0).numpy())
-        flash("Processed Successfully", "p")
-        path_to_save = [orig_path_to_save, gen_path_to_save]
-        return render_template('upload.html', img_path=path_to_save)
+            # return path_to_save[1]
+            return render_template('upload.html', img_path=path_to_save)
+
+        else:
+            return "'photo' not found in form-data!!"
+
+    # return prediction
+    print("Its a GET request!!")
     return render_template('upload.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True, threaded=True)
-    # app.run()
+    # app.run(debug=True, use_reloader=True, threaded=True)
+    app.run()
